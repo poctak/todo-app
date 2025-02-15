84ffac8cfb4f3ce91a4fb509e0fa9d5085d784ff
TomPocta <poctakt@seznam.cz>
2025-02-15 08:55:52 +0100

    Oprava názvu

--- gui.py
+++ gui.py
@@ -2,8 +2,8 @@ import functions
 import FreeSimpleGUI as sg
 label = sg.Text("Type in a todo")
 input_box = sg.InputText('Enter todo')
-buttonOk = sg.Button('Add')
+add_button = sg.Button('Add')
 
-window = sg.Window("Moje aplikace",layout=[[label],[ input_box, buttonOk ]])
+window = sg.Window("Moje aplikace",layout=[[label],[ input_box, add_button ]])
 window.read()
 window.close()
